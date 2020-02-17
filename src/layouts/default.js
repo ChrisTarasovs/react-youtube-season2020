928e466742fe3d194775ace3d5177c326a68d988
chris@frontr.co.uk <chris@frontr.co.uk>
2020-02-17 12:53:11 +0000

    introducing routing in reactjs

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import Header from '../containers/header';
 import Footer from '../containers/footer';
 import Sidebar from '../containers/sidebar';
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Dashboard from '../features/dashboard';
+import User from '../features/user';
 class DefaultLayout extends Component{
     render(){
         return(
             <div>
                 <Header/>
-                <div>Body data</div> <Sidebar />
+                <Router>
+                    <Link to={'/dashboard'}>Dashboard</Link>
+                    <br />
+                    <Link to={'/profile'}>Profile</Link>
+                    <Route path="/dashboard" exact component={Dashboard} />
+                    <Route path="/profile" exact component={User} />
+                </Router>
+                <Sidebar />
                 <Footer/>
             </div>
         )
