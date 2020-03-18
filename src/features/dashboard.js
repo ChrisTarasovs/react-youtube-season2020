efd5715bb6762b1780ee4e5cfcdae9c3a1973b38
chris@frontr.co.uk <chris@frontr.co.uk>
2020-03-18 12:37:35 +0000

    introducing api call

--- src/features/dashboard.js
+++ src/features/dashboard.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-class Dashboard extends Component{
-    render(){
-        return (
-            <div>Dashboard works</div>
-        )
-    }
+function Dashboard() {
+    const [employees, setEmployees] = useState([]);
+    useEffect(()=>{
+        fetch('http://dummy.restapiexample.com/api/v1/employees')
+        .then( res => res.json())
+        .then( responce =>{
+           setEmployees(responce['data'])
+        })
+    })
+    const employeeList = employees.map( (item, index) => {
+        return <div key={index}>{item.employee_name}</div>
+    })
+    return (
+    <div>Dashboard works {employeeList}</div>
+    )
+  
 }
 
 export default Dashboard;
